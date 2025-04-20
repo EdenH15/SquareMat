60c7245a225f37e0bbfe037adcc1ec04be32698d
Eden <edenhassin@gmail.com>
2025-04-20 14:26:12 +0300

    new

---
+++ SquareMat.cpp
@@ -0,0 +1,14 @@
+//
+// Created by eden on 4/20/25.
+// Edenhassin@gmail.com
+
+
+namespace Matrix {
+
+    class SquareMat {
+        public:
+        SquareMat(int rows, int cols) {}
+    }
+
+
+}
---
+++ SquareMat.h
@@ -0,0 +1,63 @@
+//
+// Created by eden on 4/20/25.
+//
+
+#ifndef SQUAREMAT_H
+#define SQUAREMAT_H
+#include <utility>
+#include <iostream>
+
+
+namespace Matrix {
+
+    class SquareMat {
+        private:
+        int size;
+        double *data;
+
+        public:
+            SquareMat(int n);
+            SquareMat(const SquareMat& other); // Copy Constructor
+            SquareMat& operator=(const SquareMat& other); // Copy Assignment Operator
+            ~SquareMat();
+
+            SquareMat operator+(const SquareMat& other) const;
+            SquareMat operator-(const SquareMat& other) const;
+            SquareMat operator-() const;
+            SquareMat operator*(const SquareMat& other) const;
+            SquareMat operator*(double s) const;
+            SquareMat operator%(const SquareMat& other) const;
+            SquareMat operator%(int s) const;
+            SquareMat operator/(double s) const;
+            SquareMat operator^(int p) const;
+            SquareMat& operator++();
+            SquareMat operator++(int);
+            SquareMat& operator--();
+            SquareMat operator--(int);
+            SquareMat operator~() const;
+            double operator[](std::pair<int,int>) const;
+            double& operator[](std::pair<int,int>);
+            bool operator==(const SquareMat& other) const;
+            bool operator!=(const SquareMat& other) const;
+            bool operator<(const SquareMat& other) const;
+            bool operator>(const SquareMat& other) const;
+            bool operator<=(const SquareMat& other) const;
+            bool operator>=(const SquareMat& other) const;
+            double operator!() const;
+            SquareMat& operator+=(const SquareMat& other);
+            SquareMat& operator-=(const SquareMat& other);
+            SquareMat& operator*=(const SquareMat& other);
+            SquareMat& operator/=(const SquareMat& other);
+            SquareMat& operator%=(const SquareMat& other);
+
+            friend std::ostream& operator<<(std::ostream& os, const SquareMat& mat);
+
+
+
+    };
+
+
+}
+
+
+#endif //SQUAREMAT_H
